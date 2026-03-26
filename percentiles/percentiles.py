95c9c87af8db013339eaed3fcdcbcc953c10fc84
Phan Minh Khoi <pmkhoi2420@apcs.fitus.edu.vn>
2026-03-26 21:36:54 +0700

    Add solution for percentiles

---
+++ percentiles/percentiles.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def percentiles(x, q):
+    """
+    Compute percentiles using linear interpolation.
+    """
+    x = np.array(x)
+    q = np.array(q)
+
+    ans = np.percentile(x,q, method="linear")
+    return ans
